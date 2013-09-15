4ee94ced5b028bf3f829fa66b21cf6b3d2b3069e
Matthew McEachen <matthew+github@mceachen.org>
2013-09-15 11:27:46 -0700

    fix test to handle git's mtime infidelity

--- test/lib/proto_asset_test.rb
+++ test/lib/proto_asset_test.rb
@@ -1,11 +1,18 @@
-require "test_helper"
+require 'test_helper'
 
 describe ProtoAsset do
   before :each do
-    @pa = ProtoAsset.new(img_path("IMG_2452.jpg"))
+    @pa = ProtoAsset.new(img_path('IMG_2452.jpg'))
   end
 
   it '#urns works' do
-    @pa.urns.must_equal(URN::FsAttrs=>"urn:fs-attrs:1379104396:2940995", URN::Sha1=>"urn:sha1:026f5bac679f5373cd8503ca43586314d851eebb", URN::Exif=>"urn:exif:1309970805:2.8:160:2.8:1/800:Canon:Canon PowerShot SD980 IS")
+    actual = @pa.urns
+    expected = {
+      URN::Sha1 => 'urn:sha1:026f5bac679f5373cd8503ca43586314d851eebb',
+      URN::Exif => 'urn:exif:1309970805:2.8:160:2.8:1/800:Canon:Canon PowerShot SD980 IS'
+    }
+    actual.except(URN::FsAttrs).must_equal_hash(expected)
+    # Filesystems may have different mtimes:
+    actual[URN::FsAttrs].must_match /urn:fs-attrs:\d{10}:2940995/
   end
 end
